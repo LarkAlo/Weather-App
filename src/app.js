b3716476d4564b894e390c0103fb8fb7ba179558
LarkAlo <118227350+LarkAlo@users.noreply.github.com>
2022-12-15 17:20:42 -0700

    updates
    updates in humidity/wind

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//FormattedDate
+//formatDate
 function formatDate() {
   let now = new Date();
   let days = [
@@ -24,77 +24,7 @@ function formatDate() {
 }
 formatDate();
 
-//Weather
-function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
-
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      coordinates: response.data.coordinates,
-      temperature: response.data.temperature.current,
-      humidity: response.data.temperature.humidity,
-      date: new Date(response.data.time * 1000),
-      description: response.data.condition.description,
-      icon: response.data.condition.icon,
-      wind: response.data.wind.speed,
-      city: response.data.city,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
-
-  function search() {
-    const apiKey = "be60748992fab0f5da8162563fb21245";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9 ">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control search-input"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3 p-0">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <WeatherInfo data={weatherData} />
-        <WeatherForecast
-          coordinates={weatherData.coordinates}
-          city={weatherData.city}
-        />
-      </div>
-    );
-  } else {
-    search();
-    return "Loading...";
-  }
-}
-
-//Search
+//searchCity
 function searchCity(event) {
   event.preventDefault();
   let currentCity = document.querySelector("#search-input");
@@ -107,7 +37,7 @@ searchForm.addEventListener("submit", searchCity);
 //SearchCity
 function searchCity(event) {
   event.preventDefault();
-  let apiKey = "667d9f573c8af4c33457be5d561a9148";
+  let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
   let cityName = document.querySelector("#search-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
@@ -117,46 +47,7 @@ function searchCity(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
 
-//WeatherForecast
-function WeatherForecast(props) {
-  const [loaded, setLoaded] = useState(false);
-  const [forecast, setForecast] = useState(null);
-
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
-
-  function handleForecastResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
-
-  if (loaded) {
-    return (
-      <div className="WeatherForecast row">
-        {forecast.map(function (day, index) {
-          if (index < 5) {
-            return (
-              <div className="col" key={index}>
-                <WeatherForecastPreview data={day} />
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </div>
-    );
-  } else {
-    let apiKey = "be60748992fab0f5da8162563fb21245";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleForecastResponse);
-
-    return null;
-  }
-}
-//show weather
+//showWeather
 function showWeather(response) {
   document.querySelector("#currentCity").innerHTML = response.data.name;
   document.querySelector("#description").innerHTML =
@@ -164,74 +55,8 @@ function showWeather(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-  document.querySelector("#Humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#Wind").innerHTML = Math.round(
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
-  );
-}
-
-//WeatherForecastPreview
-function WeatherForecastPreview(props) {
-  function day() {
-    let date = new Date(props.data.time * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    return days[day];
-  }
-
-  function maxTemperature() {
-    let temperature = Math.round(props.data.temperature.maximum);
-
-    return `${temperature}°`;
-  }
-
-  function minTemperature() {
-    let temperature = Math.round(props.data.temperature.minimum);
-
-    return `${temperature}°`;
-  }
-
-  return (
-    <div className="WeatherForecastPreview">
-      <div className="forecast-time">{day()}</div>
-      <WeatherIcon code={props.data.condition.icon} size={38} />
-      <div className="forecast-temperature">
-        <span className="forecast-temperature-max">{maxTemperature()}</span>
-        <span className="forecast-temperature-min">{minTemperature()}</span>
-      </div>
-    </div>
-  );
-}
-
-//WeatherInfo
-function WeatherInfo(props) {
-  return (
-    <div className="WeatherInfo">
-      <div className="row">
-        <div className="col-6">
-          <h1>{props.data.city}</h1>
-          <ul>
-            <li>
-              <FormattedDate date={props.data.date} />, {props.data.description}
-            </li>
-            <li>
-              Humidity: <strong>{props.data.humidity}%</strong>, Wind:{" "}
-              <strong>{props.data.wind}km/h</strong>
-            </li>
-          </ul>
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-8">
-          <div className="temperature-container d-flex justify-content-end">
-            <WeatherIcon code={props.data.icon} size={52} />
-            <div>
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
