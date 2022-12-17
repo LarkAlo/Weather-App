d15527329410e883ade1cc0cb560acb57091e5c2
LarkAlo <118227350+LarkAlo@users.noreply.github.com>
2022-12-17 13:34:20 -0700

    F to C
    unit conversions

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ form.addEventListener("submit", searchCity);
 
 //showWeather
 function showWeather(response) {
+  console.log(response);
   let iconElement = document.querySelector("#icon");
   document.querySelector("#currentCity").innerHTML = response.data.name;
   document.querySelector("#description").innerHTML =
@@ -64,6 +65,33 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  celsiusTemperature = response.data.main.temp;
 }
+
+//units conversion
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#currentTemp");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("actice");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("currentTemp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 //weatherforecast preview
